5b713b6af831a5e2e853039a346c0de0c8607069
Gavin Atkinson <gatkinso@risd.edu>
2017-06-12 21:32:25 -0400

    sketch

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header> <h1>{'gt'}</h1> </header>
+      <main>
+        <ul>
+          <li>
+            <div className={'oval'} />
+            <div className={'num'}> {'01'} </div>
+            <h3>{'Arena TV'}</h3>
+            <img src={'http://printer.generaltrademark.com/printer.jpg'}/>
+            <img src={'http://generaltrademark.com/package4-copy.jpg'}/>
+            <img src={'http://generaltrademark.com/rock.jpg'}/>
+            <img src={'http://printer.generaltrademark.com/printer.jpg'}/>
+            <img src={'http://generaltrademark.com/package4-copy.jpg'}/>
+            <img src={'http://generaltrademark.com/rock.jpg'}/>
+            <img src={'http://printer.generaltrademark.com/printer.jpg'}/>
+            <img src={'http://generaltrademark.com/package4-copy.jpg'}/>
+            <img src={'http://generaltrademark.com/rock.jpg'}/>
+          </li>
+          <li>
+          <div className={'oval'} />
+          <div className={'num'}> {'02'} </div>
+          <h3>{'Printer M―01'}</h3>
+          <img src={'http://appliancecenter.com/wp-content/uploads/2014/04/Washing-Machine-Spare-Parts-2.jpg'}/>
+          <img src={'http://generaltrademark.com/package4-copy.jpg'}/>
+          <img src={'http://generaltrademark.com/rock.jpg'}/>
+          <img src={'http://printer.generaltrademark.com/printer.jpg'}/>
+          </li>
+          <li>
+          <div className={'oval'} />
+
+          <div className={'num'}> {'03'} </div>
+          <h3>{'P―Nodes'}</h3>
+          <img src={'http://pneu.haus/content/1-projects/2-camera-obscura/01.jpg'}/>
+
+          </li>
+        </ul>
+      </main>
+      </div>
+    );
+  }
+}
+
+export default App;
